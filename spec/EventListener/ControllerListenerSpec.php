a313e8b742e391444e658f5432e14830a589f0fa
mamazu <maxo.pech@gmail.com>
2018-12-16 19:52:37 +0100

    Added more spec test and fixing texts

---
+++ spec/EventListener/ControllerListenerSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace spec\Setono\SyliusRedirectPlugin\EventListener;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use PhpSpec\ObjectBehavior;
+use Setono\SyliusRedirectPlugin\EventListener\ControllerListener;
+use Setono\SyliusRedirectPlugin\Repository\RedirectRepositoryInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Setono\SyliusRedirectPlugin\Model\Redirect;
+use Setono\SyliusRedirectPlugin\Model\RedirectInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Prophecy\Argument;
+
+class ControllerListenerSpec extends ObjectBehavior
+{
+	function let(
+		RedirectRepositoryInterface $redirectRepository,
+		ObjectManager $objectManager
+	) : void {
+		$this->beConstructedWith($redirectRepository, $objectManager);
+	}
+
+	function it_is_a_controller_listener() : void
+	{
+		$this->shouldHaveType(ControllerListener::class);
+	}
+
+	function it_redirects_to_the_correct_controller(
+		FilterControllerEvent $filterControllerEvent,
+		Request $request,
+		RedirectRepositoryInterface $redirectRepository,
+		ObjectManager $objectManager,
+		RedirectInterface $redirect
+	) : void {
+		$filterControllerEvent->getRequest()->willReturn($request);
+		$request->getPathInfo()->willReturn('/home');
+
+		$redirectRepository->findEnabledBySource('/home', false)->willReturn($redirect);
+		$redirect->onAccess()->shouldBeCalled();
+		$objectManager->flush()->shouldBeCalled();
+
+		$redirectRepository->findLastRedirect($redirect, false)->willReturn($redirect);
+		$filterControllerEvent->setController(Argument::type(\Closure::class))->shouldBeCalled();
+
+		$this->onKernelController($filterControllerEvent);
+	}
+
+	function it_does_not_redirect_if_there_is_no_enabed_redirect(
+		FilterControllerEvent $filterControllerEvent,
+		Request $request,
+		RedirectRepositoryInterface $redirectRepository,
+		ObjectManager $objectManager,
+		RedirectInterface $redirect
+	) : void {
+		$filterControllerEvent->getRequest()->willReturn($request);
+		$request->getPathInfo()->willReturn('/home');
+
+		$redirectRepository->findEnabledBySource('/home', false)->willReturn($redirect);
+		$redirect->onAccess()->shouldNotBeCalled();
+	}
+}
--- spec/Validator/Constraints/InfiniteLoopValidatorSpec.php
+++ spec/Validator/Constraints/InfiniteLoopValidatorSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace spec\Setono\SyliusRedirectPlugin\Validator\Constraints;
 
@@ -12,58 +12,65 @@ use Setono\SyliusRedirectPlugin\Validator\Constraints\InfiniteLoop;
 use Symfony\Component\Validator\ConstraintValidatorInterface;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 final class InfiniteLoopValidatorSpec extends ObjectBehavior
 {
-    public function let(ExecutionContextInterface $context, RedirectRepositoryInterface $redirectRepository): void
+    public function let(ExecutionContextInterface $context, RedirectRepositoryInterface $redirectRepository) : void
     {
         $this->beConstructedWith($redirectRepository);
         $this->initialize($context);
     }
 
-    public function it_is_constraint_validator(): void
+    public function it_is_constraint_validator() : void
     {
         $this->shouldHaveType(ConstraintValidatorInterface::class);
     }
 
-    public function it_does_not_apply_to_null_values(ExecutionContextInterface $context): void
+    public function it_does_not_apply_to_null_values(ExecutionContextInterface $context) : void
     {
-        $context->addViolation(Argument::cetera())->shouldNotBeCalled();
+        $context->buildViolation(Argument::any())->shouldNotBeCalled();
 
         $this->validate(null, new InfiniteLoop());
     }
 
-    public function it_throws_an_exception_if_subject_is_not_a_string(ExecutionContextInterface $context): void
+    public function it_throws_an_exception_if_subject_is_not_a_string(ExecutionContextInterface $context) : void
     {
-        $context->addViolation(Argument::cetera())->shouldNotBeCalled();
+        $context->buildViolation(Argument::any())->shouldNotBeCalled();
 
         $this->shouldThrow(UnexpectedTypeException::class)->during('validate', [new \stdClass(), new InfiniteLoop()]);
     }
 
-    /*
     public function it_adds_violation_if_subject_will_create_infinite_loop(
         ExecutionContextInterface $context,
         RedirectRepositoryInterface $redirectRepository,
-        RedirectInterface $subject
-    ): void {
-        $subject->isEnabled()->shouldBeCalled()->willReturn(true);
-        $subject->getSource()->shouldBeCalled()->willReturn('/source');
+        RedirectInterface $subject,
+        RedirectInterface $nextRedirect,
+        ConstraintViolationBuilderInterface $violationBuilder
+    ) : void {
+        $subject->isEnabled()->willReturn(true);
+        $subject->getSource()->willReturn('/source');
 
-        $context->addViolation(Argument::cetera())->shouldBeCalled();
+        $redirectRepository->searchNextRedirect($subject)->willReturn($nextRedirect);
+        $nextRedirect->getDestination()->willReturn('/source');
+
+        $context->buildViolation('setono_sylius_redirect.form.errors.target_result_in_infinite_loop')
+            ->willReturn($violationBuilder);
+        $violationBuilder->atPath('destination')->willReturn($violationBuilder);
+        $violationBuilder->addViolation()->shouldBeCalled();
 
         $this->validate($subject, new InfiniteLoop());
     }
 
-    /*
-    function it_does_not_add_violation_if_subject_is_enabled(
+    function it_does_not_add_violation_if_subject_is_disabled(
         ExecutionContextInterface $context,
-        ToggleableInterface $subject
-    ): void {
-        $subject->isEnabled()->shouldBeCalled()->willReturn(true);
+        RedirectInterface $subject
+    ) : void {
+        $subject->isEnabled()->willReturn(false);
 
-        $context->addViolation(Argument::cetera())->shouldNotBeCalled();
+        $context->buildViolation(Argument::any())->shouldNotBeCalled();
 
-        $this->validate($subject, new Enabled());
+        $this->validate($subject, new InfiniteLoop());
     }
-    */
 }
