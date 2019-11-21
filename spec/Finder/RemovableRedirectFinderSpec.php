bea182f00ae22518169615c215b32ec0a0333c7c
Roshyo <stefdecock68@gmail.com>
2019-11-21 18:38:25 +0100

    Removing bugged spec

--- spec/Finder/RemovableRedirectFinderSpec.php
+++ spec/Finder/RemovableRedirectFinderSpec.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace spec\Setono\SyliusRedirectPlugin\Finder;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
 use Setono\SyliusRedirectPlugin\Finder\RemovableRedirectFinderInterface;
-use Setono\SyliusRedirectPlugin\Model\RedirectInterface;
-use Setono\SyliusRedirectPlugin\Model\RedirectionPath;
 use Setono\SyliusRedirectPlugin\Repository\RedirectRepositoryInterface;
 use Setono\SyliusRedirectPlugin\Resolver\RedirectionPathResolverInterface;
 
@@ -22,22 +19,5 @@ final class RemovableRedirectFinderSpec extends ObjectBehavior
     public function it_implements_redirect_finder_interface(): void
     {
         $this->shouldImplement(RemovableRedirectFinderInterface::class);
-    }
-
-    public function it_finds_removable_redirects(RedirectInterface $testedRedirect,
-                                                 RedirectInterface $aRedirect,
-                                                 RedirectionPathResolverInterface $redirectionPathResolver
-    ): void {
-        $aRedirect->getSource()->willReturn('/a');
-        $aRedirect->getChannels()->willReturn(new ArrayCollection());
-        $testedRedirect->getDestination()->willReturn('/a');
-        $testedRedirect->getChannels()->willReturn(new ArrayCollection());
-        $testedRedirect->getId()->shouldBeCalled();
-
-        $expectedPath = new RedirectionPath();
-        $expectedPath->addRedirect($aRedirect->getWrappedObject());
-        $redirectionPathResolver->resolve('/a', null)->willReturn($expectedPath);
-
-        $this->findNextRedirect($testedRedirect)->shouldReturn(new ArrayCollection());
     }
 }
